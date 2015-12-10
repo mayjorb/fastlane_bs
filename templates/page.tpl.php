7d1ac519afc6b869654f10f331f9063d3270ee11
Bryan Mayjor <bmayjor@gmail.com>
2015-12-10 10:49:13 -0800

    7.x-1.1 - added favicons to html.tpl.php

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -78,7 +78,7 @@
     <div class="navbar-header">
       <?php if ($logo): ?>
         <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          <img src="<?php print $logo; ?>" width="235px" height="34px" alt="<?php print t('Home'); ?>" />
         </a>
       <?php endif; ?>
 
